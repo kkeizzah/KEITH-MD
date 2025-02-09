263044292dfad1f3b34c96b96c76174edb004c38
kkeizzah <keizzahketh@gmail.com>
2025-02-09 15:01:57 +0300

    Update upload.js

--- Cmds/Media/upload.js
+++ Cmds/Media/upload.js
@@ -1,28 +1,31 @@
 
+const fs = require("fs");
+const path = require("path");
+const util = require("util");
+
 module.exports = async (context) => {
-    const { client, m, uploadtoimgur } = context;
-    const fs = require("fs");
-    const path = require('path');
-    const util = require("util");
+  const { client, m, uploadtoimgur } = context;
 
-    let q = m.quoted ? m.quoted : m;
-    let mime = (q.msg || q).mimetype || '';
+  let q = m.quoted ? m.quoted : m;
+  let mime = (q.msg || q).mimetype || "";
 
-    if (!mime) return m.reply('Quote an image or video');
+  if (!mime) return m.reply('Quote an image, video, or other media');
 
-    let mediaBuffer = await q.download();
+  let mediaBuffer = await q.download();
 
-    if (mediaBuffer.length > 10 * 1024 * 1024) return m.reply('Media is too large.');
+  if (mediaBuffer.length > 10 * 1024 * 1024) return m.reply('Media is too large.');
 
-    let isTele = /image\/(png|jpe?g|gif)|video\/mp4|audio\/(mpeg|ogg|wav)|application\/octet-stream/.test(mime);
+  // Check if the media type is supported
+  let isSupported = /image\/(png|jpe?g|gif)|video\/mp4|audio\/.*/.test(mime);
 
-    if (isTele) {
-        let filePath = await client.downloadAndSaveMediaMessage(q);
-        let link = await uploadtoimgur(filePath);
+  if (isSupported) {
+    let fta2 = await client.downloadAndSaveMediaMessage(q);
+    let link = await uploadtoimgur(fta2);
 
-        const fileSizeMB = (mediaBuffer.length / (1024 * 1024)).toFixed(2);
-        m.reply(`Media Link:-\n\n${link}`);
-    } else {
-        m.reply('Error occurred...');
-    }
+    const fileSizeMB = (mediaBuffer.length / (1024 * 1024)).toFixed(2);
+
+    m.reply(`Media Link:\n\n${link}\n\nFile Size: ${fileSizeMB} MB`);
+  } else {
+    m.reply('Error occurred. Unsupported media type.');
+  }
 };
