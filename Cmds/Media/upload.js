e753c517c169e128b4713472a72b6badf3f1d150
kkeizzah <keizzahketh@gmail.com>
2025-02-09 17:35:27 +0300

    Update upload.js

--- Cmds/Media/upload.js
+++ Cmds/Media/upload.js
@@ -1,30 +1,41 @@
+module.exports = async (context) => {
+    const { client, m, uploadtoimgur } = context;
+ const fs = require("fs");
+const path = require('path');
 
-const fs = require("fs");
-const path = require("path");
 const util = require("util");
 
-module.exports = async (context) => {
-  const { client, m, uploadToCatbox } = context;
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || ''
 
-  let q = m.quoted ? m.quoted : m;
-  let mime = (q.msg || q).mimetype || '';
+if (!mime) return m.reply('Quote an image or video')
 
-  if (!mime) return m.reply('Quote an image or video');
+let mediaBuffer = await q.download()
 
-  let mediaBuffer = await q.download();
+  if (mediaBuffer.length > 10 * 1024 * 1024) return m.reply('Media is too large.')
 
-  if (mediaBuffer.length > 10 * 1024 * 1024) return m.reply('Media is too large.');
 
-  let isSupported = /image\/(png|jpe?g|gif)|video\/mp4|audio\/.*/.test(mime);
 
-  if (isSupported) {
-    let fta2 = await client.downloadAndSaveMediaMessage(q);
-    let link = await uploadToCatbox(fta2);
 
-    const fileSizeMB = (mediaBuffer.length / (1024 * 1024)).toFixed(2);
+let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
 
-    m.reply(`Media Link:\n\n${link}\n\nFile Size: ${fileSizeMB} MB`);
+
+if (isTele) {
+
+let fta2 = await client.downloadAndSaveMediaMessage(q)
+
+    let link = await uploadtoimgur(fta2)
+
+    const fileSizeMB = (mediaBuffer.length / (1024 * 1024)).toFixed(2)
+
+    m.reply(`Media Link:-\n\n${link}`)
   } else {
-    m.reply('Error occurred. Unsupported media type.');
+    m.reply(`Error occured...`)
   }
-};
+              
+      
+          
+                
+
+
+            }
